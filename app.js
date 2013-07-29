7d398d8fc18d4d49de936a6da559f0bd4a71c1fa
Aaron Ogle <aaron@geekgonecrazy.com>
2013-07-29 16:08:49 -0500

    About to implement websocket. Possibly the most complicated part

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ message = 'HTTP/1.1 200 OK\n'+
 'CACHE-CONTROL: max-age=1800\n'+
 'CONFIGID.UPNP.ORG: 7337\n'+
 'BOOTID.UPNP.ORG: 7337\n'+
-'USN: uuid:'+uuid.v5({ns: uuid.ns.DNS, data: "testies"})+
+'USN: uuid:'+uuid.v5({ns: uuid.ns.DNS, data: "test"})+
 '\nST: urn:dial-multiscreen-org:service:dial:1\n\n';
 console.log(message);
 ssdp.on('listening', function () {
@@ -34,10 +34,18 @@ ssdp.bind(1900, function(){
 
 var express = require('express');
 var app = express();
+var http = require('http');
+server = http.createServer(app);
+var active_app = 'YouTube';
+
+app.use(express.bodyParser());
 
 app.use(function(req, res, next){
     //console.log(req.headers['user-agent']);
     console.log(req.method, req.url);
+    if (req.method == 'POST') {
+    	console.log(req.body);
+    }
     next();
 });
 
@@ -51,10 +59,10 @@ app.get('/ssdp/device-desc.xml', function(req, res) {
         '<URLBase>http://192.168.1.22:8008</URLBase>'+
         '<device>'+
             '<deviceType>urn:schemas-upnp-org:device:dail:1</deviceType>'+
-            '<friendlyName>testies</friendlyName>'+
+            '<friendlyName>test</friendlyName>'+
             '<manufacturer>Google Inc.</manufacturer>'+
             '<modelName>Eureka Dongle</modelName>'+
-            '<UDN>uuid:'+uuid.v5({ns: uuid.ns.DNS, data: "testies"})+'</UDN>'+
+            '<UDN>uuid:'+uuid.v5({ns: uuid.ns.DNS, data: "test"})+'</UDN>'+
             '<serviceList>'+
                 '<service>'+
                    '<serviceType>urn:schemas-upnp-org:service:dail:1</serviceType>'+
@@ -75,29 +83,77 @@ app.get('/ssdp/device-desc.xml', function(req, res) {
     res.send(body);
 });
 
-service('c06ac0a4-95e9-4c68-83c5-75e3714ec409');
-service('YouTube');
+app.get('/apps', function(req, res) {
+	if (active_app) {
+		res.redirect('/apps/'+active_app);
+	}
+});
+
+var services = [];
+services['c06ac0a4-95e9-4c68-83c5-75e3714ec409'] = new service('c06ac0a4-95e9-4c68-83c5-75e3714ec409', 'http://labs.geekgonecrazy.com/chromecast/receiver.html');
+services['YouTube'] = new service('YouTube');
+services['ChromeCast'] = new service('ChromeCast');
 
 function service(name, url, protocols) {
-	var body = '<?xml version="1.0" encoding="UTF-8"?>'+
-	'<service xmlns="urn:dial-multiscreen-org:schemas:dial">'+
-	  '<name>'+name+'</name>'+
-	  '<options allowStop="true"/>'+
-	  '<state>stopped</state>'+
-	'</service>';
+	this.running = false;
+	this.runningText = 'stopped';
+	this.name = name;
+	this.url = url;
 
-	app.get('/apps/'+name, function(req, res) {
-		res.setHeader('Access-Control-Allow-Method', 'GET, POST, DELETE, OPTIONS');
-	    res.setHeader('Access-Control-Expose-Headers', 'Location');
-	    res.setHeader('Application-URL', 'http://192.168.1.22:8008/apps');
-	    res.setHeader('Content-Type', 'application/xml');
-	    res.setHeader('Cache-control', 'no-cache, must-revalidate, no-store');
-	    res.send(body);
-	});
-	
+	this.getBody = function() {
+		var body = '<?xml version="1.0" encoding="UTF-8"?>'+
+		'<service xmlns="urn:dial-multiscreen-org:schemas:dial">'+
+		  '<name>'+this.name+'</name>'+
+		  '<options allowStop="true"/>'+
+		  '<state>'+this.runningText+'</state>';
+		  if (this.running) {
+		  	body += '<link rel="run" href="run" />';
+		  }
 
+		body += '</service>';
+		return body.toString();
+	}
+
+	this.start = function() {
+		this.running = true;
+		this.runningText = 'running';
+		return this.getBody();
+	}
+
+	this.stop = function() {
+		this.running = false;
+		this.runningText = 'stopped';
+	}
+
+	this.launchChrome = function() {
+
+	}
 }
 
-app.listen(8008);
+app.get('/apps/:name', function(req, res) {
+	res.setHeader('Access-Control-Allow-Method', 'GET, POST, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Expose-Headers', 'Location');
+    res.setHeader('Application-URL', 'http://192.168.1.22:8008/apps');
+    res.setHeader('Content-Type', 'application/xml');
+    res.setHeader('Cache-control', 'no-cache, must-revalidate, no-store');
+    res.send(services[req.params.name].getBody());
+});
+
+app.post('/apps/:name', function(req, res) {
+	res.setHeader('Access-Control-Allow-Method', 'GET, POST, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Expose-Headers', 'Location');
+    res.setHeader('Application-URL', 'http://192.168.1.22:8008/apps');
+    res.setHeader('Content-Type', 'application/xml');
+    res.setHeader('Cache-control', 'no-cache, must-revalidate, no-store');
+    res.send(services[req.params.name].start());
+});
+
+
 console.log('Listening on port 8008');
+
+
+server.listen(8008, function() {
+    console.log((new Date()) + ' Server is listening on port 8008');
+});
+
 
